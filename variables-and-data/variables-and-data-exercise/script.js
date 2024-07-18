0b79273a8cb46cfe701e4bb8a58f42e63628970b
wintonsec <wfong3773@gmail.com>
2024-07-17 17:53:46 -1000

    Completed Variables-and-Data assignment

--- variables-and-data/variables-and-data-exercise/script.js
+++ variables-and-data/variables-and-data-exercise/script.js
@@ -1,1 +1,52 @@
 // Variables and Data Exercises here
+let fullName = "Winton Fong";
+// Logs "Winton Fong"
+console.log(fullName);
+// Variables and Data Exercise: Strings and Numbers
+console.log(typeof "fullName"); // logs "string"
+
+const sentence = "Winton Fong";
+
+// toUpperCase()
+console.log(sentence.toUpperCase());
+// Expected output: "WINTON FONG"
+
+// toLowerCase()
+console.log(sentence.toLowerCase());
+// Expected output; "winton fong"
+
+let stringAge = "28";
+let ageIn2Years = parseInt(stringAge) + 2;
+
+console.log(ageIn2Years);
+// Expected output; "In 2 years, you will be 30"
+console.log(typeof "ageIn2Years");
+
+let charge = 3;
+let payment = 2;
+remaining = charge - payment;
+console.log(remaining);
+console.log(typeof remaining);
+
+let tests = 4;
+let assignments = 5;
+let final = 6;
+grade = (tests + assignments + final) / 3;
+
+console.log(grade);
+console.log(typeof grade);
+
+let likesVegetables = true;
+console.log(likesVegetables);
+console.log(typeof likesVegetables);
+
+let pH = 8;
+let isAcidic = pH < 7;
+
+console.log(isAcidic);
+console.log(typeof isAcidic);
+
+let numberOfSoldiers = 10;
+let isSparta = numberOfSoldiers == 300;
+console.log(isSparta);
+console.log(typeof isSparta);
