22fd061fbf2dad8ae8261cf6fa5e18e89fe678bd
wintonsec <wfong3773@gmail.com>
2024-08-23 12:19:51 -1000

    higher-order-programming initial commit

---
+++ higher-order-programming/higher-order-programming-exercise/script.js
@@ -0,0 +1,33 @@
+// Higher Order Programming Exercise here
+
+// Calculator Exercise:
+// Pre-defined Functions
+let addition = function (num1, num2) {
+    return num1 + num2;
+};
+
+let subtraction = function (num1, num2) {
+    return num1 - num2;
+};
+
+let multiplication = function (num1, num2) {
+    return num1 * num2;
+};
+
+let division = function (num1, num2) {
+    return num1 / num2;
+};
+
+// Define the calculator function here:
+
+// Poor man's forEach Exercise
+
+// Print Each Song Title Exercise
+// Example song objects here
+let exampleSongs = [
+    { title: "Intro", artist: "The xx", minutes: 127 },
+    { title: "Free Fallin'", artist: "Tom Petty", minutes: 256 },
+    { title: "What You Know", artist: "Two Door Cinema Club", minutes: 191 },
+    { title: "Closer", artist: "The Chainsmokers", minutes: 244 },
+    { title: "Gooey", artist: "Glass Animals", minutes: 289 },
+];
---
+++ higher-order-programming/quiz.md
@@ -0,0 +1,35 @@
+# Higher Order Programming Quiz
+
+1. What does higher order programming (or functional programming) involve?
+
+2. What is a **callback function**?
+
+3. What is one advantage of using `forEach` over a normal `for` loop when iterating over an array?
+
+4. What is it called when we assign a function to a variable declared using `let`?
+
+## Tips:
+
+1. How to write `Code Examples` in markdown
+
+**for JS**:
+
+```javascript
+let data = "Howdy";
+```
+
+**for HTML**:
+
+```html
+<div>
+    <p>This is text content</p>
+</div>
+```
+
+**for CSS**:
+
+```css
+div {
+    width: 100%;
+}
+```
